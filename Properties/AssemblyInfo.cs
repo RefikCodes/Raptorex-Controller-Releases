443a54d49d7a23fba03afe0608f0e4f5793caaa6
Refik Codes <refik@codes.dev>
2025-12-01 10:20:34 +0300

    v2.0.18 - Updated assembly version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.17.0")]
-[assembly: AssemblyFileVersion("2.0.17.0")]
+[assembly: AssemblyVersion("2.0.18.0")]
+[assembly: AssemblyFileVersion("2.0.18.0")]
